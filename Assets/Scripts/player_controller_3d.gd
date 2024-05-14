f377081d38c6b32ea6f2c053a687cf0b1a138b7f
AlmiranteFuchs <brunofuchs2002@gmail.com>
2024-05-14 04:34:12 +0000

    chore: wall climb

--- Assets/Scripts/player_controller_3d.gd
+++ Assets/Scripts/player_controller_3d.gd
@@ -5,6 +5,8 @@ extends CharacterBody3D
 @onready var character_body_3d = $"."
 @onready var p_head = $p_head
 @onready var collision_shape_3d = $CollisionShape3D
+@onready var p_chest = $p_chest
+@onready var p_legs = $p_legs
 
 ##### Constants #####
 const SPEED = 5.0
@@ -26,9 +28,9 @@ func _ready():
 
 func _physics_process(delta):
 	manage_movement(delta)
-	wall_ray_cast()
+	#wall_ray_cast()
+	climb_wall()
 	move_and_slide()
-		
 
 func _input(event):
 	# Handle the mouse movement.
@@ -39,8 +41,6 @@ func _input(event):
 		if event.is_pressed():
 			if event.keycode == KEY_ESCAPE:
 				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
-		
 	
 ###### Custom Functions #####
 func manage_movement(delta):
@@ -65,7 +65,7 @@ func manage_movement(delta):
 func wall_ray_cast():
 	var space = get_world_3d().direct_space_state
 	var target_direction = (p_head.global_transform.basis.z).normalized()
-	var angle_offset_degrees = WALL_RUN_ANGLE  # Adjust this value to change the offset angle
+	var angle_offset_degrees = WALL_RUN_ANGLE # Adjust this value to change the offset angle
 	var angle_radians = deg_to_rad(angle_offset_degrees)
 	var offset_direction = target_direction.rotated(Vector3.UP, angle_radians)
 	var offset_direction2 = target_direction.rotated(Vector3.UP, -angle_radians)
@@ -82,11 +82,10 @@ func wall_ray_cast():
 	var collision = space.intersect_ray(ray_parameters)
 	var collision2 = space.intersect_ray(ray_parameters2)
 
-
 	line(p_head.global_position, ray_start - offset_direction * WALL_RUN_RAY_LENGTH, Color.RED, 1)
 	line(p_head.global_position, ray_start - offset_direction2 * WALL_RUN_RAY_LENGTH, Color.RED, 1)
 
-	if((collision && collision2) && collision.collider == collision2.collider):
+	if ((collision&&collision2)&&collision.collider == collision2.collider):
 		line(collision.position, collision2.position, Color.GREEN, 1)
 		# Check if the player is colliding with the wall
 		for i in get_slide_collision_count():
@@ -104,11 +103,53 @@ func wall_ray_cast():
 				velocity.y = 0
 				
 				break
-		
 
-		
+var started_climbing = false
+func climb_wall():
+	# If not on wall
+	if not is_on_wall():
+		return
+	var space = get_world_3d().direct_space_state
+
+	# TODO: Save collisions points in a array, reverse it, if collided with the wall, take the first point and compare the z of the next, while it is the same do nothing
+	# If it changes, probably the point it's above the wall, where the player needs to climb, 
+	# hint: collision.position
+	# Bonus: set the player position on that point, it will look like shit but It will work for now, after we can look on interpolation or fancy x² funcs for smooooothing things ip 
+
+	var starting_pos = p_head.global_position + Vector3(0, 2, 0)
+	for i in 5:
+		starting_pos = starting_pos - Vector3(0, 0.5, 0)
+		var target_dir = (p_head.basis.z.normalized())
+		var angle_radians = deg_to_rad(50)
+		var offset_direction = target_dir.rotated(Vector3.LEFT, angle_radians)
+		var ending_end = (starting_pos - (offset_direction * WALL_RUN_RAY_LENGTH))
+
+		var ray_parameters = PhysicsRayQueryParameters3D.create(starting_pos, ending_end)
+		var collision = space.intersect_ray(ray_parameters)
+
+		line(starting_pos, ending_end, Color.RED, 1)
+
+	return ;
+	# # If not on wall
+	# if not is_on_wall():
+	# 	return
+	# # Get the height of the collided object
+	# var collision = get_slide_collision(0)
+	# if (collision == null):
+	# 	return
 	
+	# # Object
+	# var obj = collision.get_collider()
+	# var obj_y_pos = obj.global_transform.origin.y
 
+	# # Collide point?
+	# var colll = collision.get_position()
+
+	# # Player
+	# var player_y_pos = global_transform.origin.y
+	# print("obj> ", obj_y_pos)
+	# print("player> ", player_y_pos)
+	# print("on> ", colll)
 
 func turn_camera(event):
 	# Rotate the camera.
@@ -118,9 +159,8 @@ func turn_camera(event):
 		camera_anglev += changev
 		camera_3d.rotate_x(deg_to_rad(changev))
 
-
 # Debbuging 
-func line(pos1: Vector3, pos2: Vector3, color = Color.WHITE_SMOKE, persist_ms = 0.02):
+func line(pos1: Vector3, pos2: Vector3, color=Color.WHITE_SMOKE, persist_ms=0.02):
 	var mesh_instance := MeshInstance3D.new()
 	var immediate_mesh := ImmediateMesh.new()
 	var material := ORMMaterial3D.new()
